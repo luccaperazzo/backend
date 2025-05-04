ddea38eb0894b9295513876ff38b0d1d4f99146a
Ilan <mc_ilan@hotmail.com>
2025-05-04 01:52:41 -0300

    estados fuera

--- models/Reserva.js
+++ models/Reserva.js
@@ -1,18 +1,10 @@
 const mongoose = require('mongoose');
 
-const reservaSchema = new mongoose.Schema({
+const reserveSchema = new mongoose.Schema({
   cliente: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   servicio: { type: mongoose.Schema.Types.ObjectId, ref: 'Service', required: true },
-<<<<<<< HEAD
   fechareserva: { type: Date, default: Date.now }
-=======
-  fechaReserva: { type: Date, default: Date.now },
-  estado: {
-    type: String,
-    enum: ['Pendiente', 'Aceptado', 'Completado', 'Cancelado'],
-    default: 'Pendiente'
-  }
->>>>>>> f65ec3eab0f2b762d9cfb92d2d7f34b6954af84b
+
 }, { timestamps: true });
 
-module.exports = mongoose.model('reserve', reservaSchema);
+module.exports = mongoose.model('reserve', reserveSchema);
